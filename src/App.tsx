f1725a37ca14e145c1bfd8363a5139abf1c60f53
manuel rodriguez <themanumanum@gmail.com>
2023-12-04 21:28:05 +0100

    initial router config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import styled from "styled-components";
-import { Landing } from "./pages";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Dashboard, Error, Landing, Register } from "./pages";
 
 function App() {
   return (
     <>
-      <Landing />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,6 +1,7 @@
 import main from "../assets/images/main.svg";
 import Wrapper from "../assets/wrappers/LandingPage";
 import { Logo } from "../components";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
@@ -22,7 +23,9 @@ const Landing = () => {
               Jobster is a free and open source job tracking app that helps you
               stay organized and focused on your job hunt.
             </p>
-            <button className="btn btn-hero">Login/Register</button>
+            <Link to={"/register"} className="btn btn-hero">
+              Login/Register
+            </Link>
           </div>
           <img src={main} alt="job hunt" className="img main-img" />
         </div>
